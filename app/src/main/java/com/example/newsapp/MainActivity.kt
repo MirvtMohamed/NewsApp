d98720cc270ced396926c9fbb8cf671e80c61f8a
MirvtMohamed <120274557+MirvtMohamed@users.noreply.github.com>
2024-09-06 04:42:03 +0300

    navigation

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.newsapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.newsapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +19,53 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.topbar)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        // Check that the correct NavGraph is being used
         navController.setGraph(R.navigation.nav_graph)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_about -> {
+                navController.navigate(R.id.settingsFragment)
+                return true
+            }
+            R.id.item_fav -> {
+
+                navController.navigate(R.id.favoriteFragment)
+                return true
+            }
+            R.id.signOut_item -> {
+
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
+
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    private lateinit var navController: NavController
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        val binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        navController = navHostFragment.navController
+//
+//        // Check that the correct NavGraph is being used
+//        navController.setGraph(R.navigation.nav_graph)
+//    }
+//}
